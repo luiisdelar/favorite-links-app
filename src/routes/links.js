b190ddf60a397bc1d97bc24071ee684c1e2442a8
Luis <luisortegadlr@gmail.com>
2020-06-24 20:31:30 -0400

    making CRUD links

--- src/routes/links.js
+++ src/routes/links.js
@@ -10,17 +10,35 @@ router.post('/add', async (req, res) => {
     const { title, url, description } = req.body;
     const newLink = { title, url, description };
     await pool.query('insert into links set ?', [newLink]);
+    //flash(dos parametros) = 1) nombre mensaje 2)valor mensaje 
+    req.flash('success', 'Link saved successfully');
     res.redirect('/links');
 });
 
 router.get('/', async (req, res) => {
     const links = await pool.query('select * from links');
+    
     res.render('links/list', {links});
 });
 
 router.get('/delete/:id', async (req, res) => {
     const { id } = req.params;
     await pool.query('delete from links where id = ?', [id]);
+    res.redirect('/links');
+});
+
+router.get('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    //links = vector de objetos
+    const links = await pool.query('select * from links where id = ?', [id]);
+    res.render('links/edit', {link: links[0]});
+});
+
+router.post('/edit/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, description, url } = req.body;
+    const newLink = { title, description, url };
+    await pool.query('update links set ? where id = ?', [newLink, id]);
     res.redirect('/links');
 });
 
